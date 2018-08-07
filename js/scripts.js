4efac128fc8b4086e69ff175ef571b1f390695b5
Kendra Kelly and Nikki Wong <nikki@>
2018-08-07 11:38:51 -0700

    set up initial code and add flashcard interactivity

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(function() {
+  $(".javascript").click(function() {
+    $("#javascript-term").toggle();
+    $("#javascript-def").toggle();
+  })
+
+  $(".operators").click(function() {
+    $("#operators-term").toggle();
+    $("#operators-def").toggle();
+  })
+
+  $(".variables").click(function() {
+    $("#variables-term").toggle();
+    $("#variables-def").toggle();
+  })
+
+  $(".variable-naming-conventions").click(function() {
+    $("#variable-naming-convention-term").toggle();
+    $("#variable-naming-conventions-def").toggle();
+  })
+
+  $(".functions").click(function() {
+    $("#functions-term").toggle();
+    $("#functions-def").toggle();
+  })
+
+  $(".methods").click(function() {
+    $("#methods-term").toggle();
+    $("#methods-def").toggle();
+  })
+
+  $(".arguments").click(function() {
+    $("#arguments-term").toggle();
+    $("#arguments-def").toggle();
+  })
+
+  $(".parameters").click(function() {
+    $("#parameters-term").toggle();
+    $("#parameters-def").toggle();
+  })
+
+  $(".return").click(function() {
+    $("#return-term").toggle();
+    $("#return-def").toggle();
+  })
+
+  $(".chaining-methods").click(function() {
+    $("#chaining-methods-term").toggle();
+    $("#chaining-methods-def").toggle();
+  })
+
+  $(".strings").click(function() {
+    $("#strings-term").toggle();
+    $("#strings-def").toggle();
+  })
+
+  $(".booleans").click(function() {
+    $("#booleans-term").toggle();
+    $("#booleans-def").toggle();
+  })
+
+  $(".undefined").click(function() {
+    $("#undefined-term").toggle();
+    $("#undefined-def").toggle();
+  })
+
+  $(".nan").click(function() {
+    $("#nan-term").toggle();
+    $("#nan-def").toggle();
+  })
+
+  $(".escape").click(function() {
+    $("#escape-term").toggle();
+    $("#escape-def").toggle();
+  })
+
+  $(".alert2").click(function() {
+    $("#alert2-term").toggle();
+    $("#alert2-def").toggle();
+  })
+
+  $(".comments").click(function() {
+    $("#comments-term").toggle();
+    $("#comments-def").toggle();
+  })
+
+  $(".jquery").click(function() {
+    $("#jquery-term").toggle();
+    $("#jquery-def").toggle();
+  })
+
+  $(".attributes").click(function() {
+    $("#attributes-term").toggle();
+    $("#attributes-def").toggle();
+  });
+});
